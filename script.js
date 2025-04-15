a43b9f7d523333118ea7a7c642c9a1abfed174e7
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-15 18:00:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let count = 0;
+function increment() {
+	alert(`button clickes ${count}`);
+	count++;
+	let counterP = document.getElementById("counter");
+	counterP.textContent = count;
+}
